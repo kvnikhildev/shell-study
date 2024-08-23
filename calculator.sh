d8a79907d650c64734eaadae904d80a168da0bff
Ubuntu <ubuntu@ip-172-31-51-10.ec2.internal>
2024-08-23 11:36:01 +0000

    the final version

---
+++ calculator.sh
@@ -0,0 +1,44 @@
+!/bin/bash
+#
+#
+#
+#
+###################
+#
+#
+#calculator 
+#
+#
+# gits 
+# ########
+#
+#  we add addition 
+#
+a+b
+
+
+####
+#
+#we added substraction 
+#
+#
+#
+a-b
+
+####
+#
+#
+#
+#we added multiplication 
+#
+#
+#
+a*b
+
+####
+#
+#
+new edit
+
+
+
